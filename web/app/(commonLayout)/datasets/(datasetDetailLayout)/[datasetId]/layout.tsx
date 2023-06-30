85fab082495a129e1fc4207b8191c6e86cca955c
GuanhaoGan <ggh21@mails.tsinghua.edu.cn>
2023-06-30 09:03:30 +0800

    私有化改造

--- web/app/(commonLayout)/datasets/(datasetDetailLayout)/[datasetId]/layout.tsx
+++ web/app/(commonLayout)/datasets/(datasetDetailLayout)/[datasetId]/layout.tsx
@@ -112,7 +112,7 @@ const DatasetDetailLayout: FC<IAppDetailLayoutProps> = (props) => {
 
   useEffect(() => {
     if (datasetRes)
-      document.title = `${datasetRes.name || 'Dataset'} - Dify`
+      document.title = `${datasetRes.name || 'Dataset'} - GFGPT`
   }, [datasetRes])
 
   const ExtraInfo: FC = () => {
