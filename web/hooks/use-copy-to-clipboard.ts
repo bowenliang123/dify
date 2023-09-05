a2108469238e5aedbf3cb947b3bbb9d4005137ac
oujianling <oujianling@gf.com.cn>
2023-09-05 21:14:00 +0800

    #12 #17 clickborad 修复以及 云上demo屏蔽

--- web/hooks/use-copy-to-clipboard.ts
+++ web/hooks/use-copy-to-clipboard.ts
@@ -9,7 +9,26 @@ function useCopyToClipboard(): [CopiedValue, CopyFn] {
     const copy: CopyFn = async text => {
         if (!navigator?.clipboard) {
             console.warn('Clipboard not supported')
-            return false
+            // Use the 'out of viewport hidden text area' trick
+            const textArea = document.createElement("textarea")
+            textArea.value = text
+                
+            // Move textarea out of the viewport so it's not visible
+            textArea.style.position = "absolute"
+            textArea.style.left = "-999999px"
+                
+            document.body.prepend(textArea)
+            textArea.select()
+
+            try {
+                document.execCommand('copy')
+            } catch (error) {
+                console.error(error)
+            } finally {
+                textArea.remove()
+            }
+            setCopiedText(text)
+            return true
         }
 
         try {
